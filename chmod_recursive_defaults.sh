9d2997259b93a0a51797370af2c405e8ea6657ee
geekgit <geeekz@gmail.com>
2013-12-15 10:05:37 +0600

    chmod defaults - fix typo

--- chmod_recursive_defaults.sh
+++ chmod_recursive_defaults.sh
@@ -2,5 +2,5 @@
 # use $1 path
 # files - 644 chmod (-rw-r--r--)
 # directory - 755 chmod (drwxr-xr-x)
-find "$1" -type f -exec chmod $2 {} \;
-find "$1" -type d -exec chmod $3 {} \;
+find "$1" -type f -exec chmod 644 {} \;
+find "$1" -type d -exec chmod 755 {} \;
