1b5493401be18b7e3c30adf43bcf70858cbd524f
geekgit <geeekz@gmail.com>
2015-06-15 18:57:06 +0500

    SHA-1 hash script

---
+++ sha1_calculate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+number_of_args="$#"
+if [ "$number_of_args" -ge "1" ];
+then
+	for file in "$@"
+	do
+		if [ -r "$file" ];
+		then
+			echo "'$file' exists and readable"
+			hashname="$file.sha1"
+			echo "$hashname"
+			pv "$file" | sha1sum > "$hashname"
+			sed -i "s/-/$file/g" "$hashname"
+			echo "Hash:"
+			cat "$hashname"
+		else
+			echo "'$file' not exist or not readable"
+		fi
+	done
+fi
