dcb5ebd18e156f9e1dfecce52574f705c90ba63d
geekgit <geeekz@gmail.com>
2015-12-15 20:58:01 +0500

    add logs

--- test-block-traffic.sh
+++ test-block-traffic.sh
@@ -4,6 +4,7 @@ id -u "$User" 1>/dev/null 2>/dev/null
 ExitCode="$?"
 if [ $ExitCode -eq 0 ]
 then
+	iptables -A OUTPUT -m owner --uid-owner "$User" -j LOG --log-prefix "[iptables-log] $User Block: "
 	iptables -A OUTPUT -m owner --uid-owner "$User" -j DROP
 	echo "OK"
 else
