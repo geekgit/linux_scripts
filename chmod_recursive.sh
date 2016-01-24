6ff755fe7cae9157d2741f43842d8e6681b854fc
geekgit <geeekz@gmail.com>
2016-01-24 20:37:36 +0500

    delete unused scripts

--- chmod_recursive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# use $1 path
-
-# set $2 rights for files
-find "$1" -type f -exec chmod $2 {} \;
-# set $3 rights for directory
-find "$1" -type d -exec chmod $3 {} \;
--- fresh_wine_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo apt-get update
-sudo apt-get install ubuntu-restricted-extras
-sudo apt-get install wine1.7
