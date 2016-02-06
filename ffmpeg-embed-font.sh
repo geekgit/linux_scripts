89c2754e9d409fad64b2f01c4bb4880c25bc1e9c
geekgit <geeekz@gmail.com>
2016-02-06 15:32:09 +0500

    ffmpeg-embed-font

---
+++ ffmpeg-embed-font.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+File="op.mkv"
+cd Fonts
+Data=$(ls)
+Dump=""
+for D in $Data
+do
+  Line="-attach ./Fonts/$D -metadata:s:t mimetype=application/x-truetype-font"
+  Dump="$Dump $Line"
+done
+echo "Dump: $Dump"
+cd ..
+ffmpeg -i $File -vcodec copy -acodec copy -f matroska $Dump "embed_$File"
+
