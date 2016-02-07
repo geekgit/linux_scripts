3d24ce9dbb5ad2ae272524637d384af2d5c35cb9
geekgit <geeekz@gmail.com>
2016-02-07 20:41:23 +0500

    ass-subtitle-analyzer update

--- ass-subtitle-analyzer.sh
+++ ass-subtitle-analyzer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-File="$1"
-Data=$(cat $File | grep 'Style:' | grep ',' | awk -F: '{print $2}' | awk -F, '{print "\"" $2 "\""}' | uniq | sort | tr "\n" " ")
-echo "Fonts: $Data"
+File="$@"
+echo "File: $File"
+SubFonts=$(cat "$File" | grep 'Style:' | grep ',' | awk -F: '{print $2}' | awk -F, '{print "\"" $2 "\""}' | sort | uniq)
+readarray -t SubFontsArray <<<"$SubFonts"
+Count=$(printf "%s\n" "${SubFontsArray[@]}"| wc -l)
+for ((i = 0 ; i < $Count ; i++)); do
+	echo "Font #$i: ${SubFontsArray[$i]}"
+done
+echo "Check availability:"
+for ((i = 0 ; i < $Count ; i++)); do
+	check-font "${SubFontsArray[$i]}"
+done
