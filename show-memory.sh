2bd61f90ef641a37b8b06c161d23e801a49d264c
geekgit <geeekz@gmail.com>
2016-02-08 15:57:30 +0500

    show-memory update

--- show-memory.sh
+++ show-memory.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
-KB_to_GB()
+function KB_to_GB
 {
-echo $(printf "%.2f" $(echo "$1/1048576.0" | bc -l | sed 's|\.|,|g'))
+	echo $(printf "%.2f" $(echo "$1/1048576.0" | bc -l | sed 's|\.|,|g'))
 }
-KB_to_MB()
+function KB_to_MB
 {
-echo $(printf "%.2f" $(echo "$1/1024.0" | bc -l | sed 's|\.|,|g'))
+	echo $(printf "%.2f" $(echo "$1/1024.0" | bc -l | sed 's|\.|,|g'))
 }
-FreeMemoryKB=$(cat /proc/meminfo | grep MemFree | awk {'print $2'})
-TotalMemoryKB=$(cat /proc/meminfo | grep MemTotal | awk {'print $2'})
-BuffersMemoryKB=$(cat /proc/meminfo | grep Buffers | awk {'print $2'})
-CachedMemoryKB=$(cat /proc/meminfo | grep -v Swap | grep Cached | awk {'print $2'})
-CalcFreeMemoryKB=$(echo "$FreeMemoryKB+$BuffersMemoryKB+$CachedMemoryKB" | bc -l)
-CalcUsedMemoryKB=$(echo "$TotalMemoryKB-$CalcFreeMemoryKB" | bc -l)
-echo "Used: $(KB_to_GB $CalcUsedMemoryKB) GB / $(KB_to_GB $TotalMemoryKB) GB"
-echo "Free: $(KB_to_GB $CalcFreeMemoryKB) GB / $(KB_to_GB $TotalMemoryKB) GB"
-
-
+function UsedLine
+{
+	FreeMemoryKB=$(cat /proc/meminfo | grep MemFree | awk {'print $2'})
+	TotalMemoryKB=$(cat /proc/meminfo | grep MemTotal | awk {'print $2'})
+	BuffersMemoryKB=$(cat /proc/meminfo | grep Buffers | awk {'print $2'})
+	CachedMemoryKB=$(cat /proc/meminfo | grep -v Swap | grep Cached | awk {'print $2'})
+	CalcFreeMemoryKB=$(echo "$FreeMemoryKB+$BuffersMemoryKB+$CachedMemoryKB" | bc -l)
+	CalcUsedMemoryKB=$(echo "$TotalMemoryKB-$CalcFreeMemoryKB" | bc -l)
+	echo "Used: $(KB_to_GB $CalcUsedMemoryKB) GB / $(KB_to_GB $TotalMemoryKB) GB"
+}
+function FreeLine
+{
+	FreeMemoryKB=$(cat /proc/meminfo | grep MemFree | awk {'print $2'})
+	TotalMemoryKB=$(cat /proc/meminfo | grep MemTotal | awk {'print $2'})
+	BuffersMemoryKB=$(cat /proc/meminfo | grep Buffers | awk {'print $2'})
+	CachedMemoryKB=$(cat /proc/meminfo | grep -v Swap | grep Cached | awk {'print $2'})
+	CalcFreeMemoryKB=$(echo "$FreeMemoryKB+$BuffersMemoryKB+$CachedMemoryKB" | bc -l)
+	CalcUsedMemoryKB=$(echo "$TotalMemoryKB-$CalcFreeMemoryKB" | bc -l)
+	echo "Free: $(KB_to_GB $CalcFreeMemoryKB) GB / $(KB_to_GB $TotalMemoryKB) GB"
+}
+function FreeInfo
+{
+	FreeMemoryKB=$(cat /proc/meminfo | grep MemFree | awk {'print $2'})
+	TotalMemoryKB=$(cat /proc/meminfo | grep MemTotal | awk {'print $2'})
+	BuffersMemoryKB=$(cat /proc/meminfo | grep Buffers | awk {'print $2'})
+	CachedMemoryKB=$(cat /proc/meminfo | grep -v Swap | grep Cached | awk {'print $2'})
+	CalcFreeMemoryKB=$(echo "$FreeMemoryKB+$BuffersMemoryKB+$CachedMemoryKB" | bc -l)
+	CalcUsedMemoryKB=$(echo "$TotalMemoryKB-$CalcFreeMemoryKB" | bc -l)
+	echo "$(KB_to_GB $CalcFreeMemoryKB) GB / $(KB_to_GB $TotalMemoryKB) GB"
+}
+FreeInfo
