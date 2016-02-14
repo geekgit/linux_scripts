9d92be899e40c4d6c81dd39e12c885758ebb0626
geekgit <geeekz@gmail.com>
2016-02-14 19:29:31 +0500

    mkv-extract-all-fonts update
    + otf support
    + some refactoring

--- mkv/mkv-extract-all-fonts.sh
+++ mkv/mkv-extract-all-fonts.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-function CountFonts
+function CountTrueTypeFonts
 {
-	echo "$(mkvmerge -i --ui-language en_US "$File" | grep Attachment | grep font | wc -l)"
+	mkvmerge -i --ui-language en_US "$File" | grep "application/x-truetype-font" | wc -l
 }
-function GetFontsID
+function CountOpenTypeFonts
 {
-	mkvmerge -i --ui-language en_US "$File" | grep Attachment | grep font | awk -F: '{print $1'} | awk '{print $3}' | tr "\n" " "
+	mkvmerge -i --ui-language en_US "$File" | grep "application/vnd.ms-opentype" | wc -l
+}
+function GetTrueTypeFontsID
+{
+	mkvmerge -i --ui-language en_US "$File" | grep "application/x-truetype-font" | awk -F: '{print $1'} | awk '{print $3}' | tr "\n" " "
+}
+function GetOpenTypeFontsID
+{
+	mkvmerge -i --ui-language en_US "$File" | grep "application/vnd.ms-opentype" | awk -F: '{print $1'} | awk '{print $3}' | tr "\n" " "
 }
 File="$1"
 echo "File: $File"
-echo "Fonts: $(CountFonts)"
-ID=$(GetFontsID)
-echo "Fonts ID: $ID"
-for FontID in $ID
+echo "TrueType fonts: $(CountTrueTypeFonts)"
+TrueTypeID=$(GetTrueTypeFontsID)
+echo "TrueType fonts ID: $TrueTypeID"
+echo "OpenType fonts: $(CountOpenTypeFonts)"
+OpenTypeID=$(GetOpenTypeFontsID)
+echo "OpenType fonts ID: $OpenTypeID"
+
+for ttf in $TrueTypeID
 do
-	echo "Font ID: $FontID"
-	mkvextract attachments "$File" "$FontID"
+	echo "ttf id: $ttf"
+	mkvextract attachments "$File" "$ttf"
+done
+
+for otf in $OpenTypeID
+do
+	echo "otf id: $otf"
+	mkvextract attachments "$File" "$otf"
 done
