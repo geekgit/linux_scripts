cc2aa1f34263ee757229b7140fb3177f55192558
geekgit <geeekz@gmail.com>
2016-02-15 16:46:45 +0500

    fontforge-show-fontname

---
+++ fonts/fontforge-show-fontname.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Usage: ./fontforge-show-fontname.sh <Font Name> <Index>
+#Example 1: ./fontforge-show-fontname.sh test.ttf 0
+ScriptName="$HOME/fontforge-show-fontname-$(uuidgen).sh"
+echo "#!/usr/bin/fontforge -lang=ff" > $ScriptName
+echo "Open(\$1)" >> $ScriptName
+echo "names = FontsInFile(\$1)" >> $ScriptName
+echo "Print(\"FontName #$2:{\"+names[$2]+\"}\")" >> $ScriptName
+echo "Quit()" >> $ScriptName
+chmod +rx "$ScriptName"
+$ScriptName "$1"
+cat $ScriptName
+rm $ScriptName
