e8720480a31eae67f1d956c33c5c964f14266727
geekgit <geeekz@gmail.com>
2016-03-13 15:42:02 +0500

    ubuntu gpg keys

---
+++ keys/key-import-ubuntu-iso.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0xFBB75451
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0xEFE21092
---
+++ keys/key-import-ubuntu-kernel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0x17C622B0
