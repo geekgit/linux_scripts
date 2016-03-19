8ca63c0452a720c19d02d9c6953dffad1a94ca4c
geekgit <geeekz@gmail.com>
2016-03-19 16:33:54 +0000

    tweak echo fix

--- tweak/tweak-disable-bluetooth-modprobe.sh
+++ tweak/tweak-disable-bluetooth-modprobe.sh
@@ -2,6 +2,6 @@
 echo "stop module"
 sudo sh -c "rmmod -f bluetooth; exit 0;"
 echo "blacklisting module"
-sudo sh -c "# Bluetooth blacklist >> /etc/modprobe.d/blacklist.conf"
-sudo sh -c "install bluetooth /bin/false >> /etc/modprobe.d/blacklist.conf"
+sudo sh -c "echo '# Bluetooth blacklist' >> /etc/modprobe.d/blacklist.conf"
+sudo sh -c "echo 'install bluetooth /bin/false' >> /etc/modprobe.d/blacklist.conf"
 sudo update-initramfs -u
