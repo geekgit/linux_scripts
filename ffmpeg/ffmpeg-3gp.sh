420c9b2da4b3e670d0c2ca1a4e6a92b37983ad58
geekgit <geeekz@gmail.com>
2016-05-02 23:49:25 +0000

    ffmpeg-3gp update

--- ffmpeg/ffmpeg-3gp.sh
+++ ffmpeg/ffmpeg-3gp.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-for File in *.mp4
+Files="$@"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
 do
-    ffmpeg -y -i $File -r 20 -s 352x288 -b 400k -acodec aac -strict experimental -ac 1 -ar 8000 -ab 24k $File-288.3gp
-    ffmpeg -y -i $File -r 20 -s 176x144 -b 200k -acodec aac -strict experimental -ac 1 -ar 8000 -ab 24k $File-144.3gp
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName144="$Plain-144.3gp"
+	NewName288="$Plain-288.3gp"
+	ffmpeg -y -i $File -r 20 -s 176x144 -b 200k -acodec aac -strict experimental -ac 1 -ar 8000 -ab 24k "$NewName144"
+	ffmpeg -y -i $File -r 20 -s 352x288 -b 400k -acodec aac -strict experimental -ac 1 -ar 8000 -ab 24k "$NewName288"
 done
