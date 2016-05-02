3a96612ab796fa5d891ad1665beef6e02c3d1577
geekgit <geeekz@gmail.com>
2016-05-02 23:47:10 +0000

    ffmpeg-mp3 update

--- ffmpeg/ffmpeg-mp3.sh
+++ ffmpeg/ffmpeg-mp3.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for File in *.mp4
+Files="$@"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
 do
-    ffmpeg -i "$File" -vn -ar 44100 -ac 2 -ab 320k -f mp3 "$File.mp3"
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName="$Plain.mp3"
+	ffmpeg -i "$File" -vn -ar 44100 -ac 2 -ab 320k -f mp3 "NewName"
 done
