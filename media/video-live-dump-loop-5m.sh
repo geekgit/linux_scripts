a138fa51f56060a6fbf706f3780311efac4d4fc5
geekgit <geeekz@gmail.com>
2016-05-03 19:05:22 +0000

    video-live-dump-loop-5m

---
+++ media/video-live-dump-loop-5m.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Url="$@"
+Timeout="5m"
+while true
+do
+	UUID=$(uuidgen)
+	Date=$(date "+%d.%m (%Hh%Mm)")
+	Filename="live-$Date-$UUID.mp4"
+	echo "Saving $Url video to file $Filename..."
+	youtube-dl --no-part --hls-use-mpegts "$Url" --output "$Filename"
+	echo "Wait $Timeout..."
+	sleep "$Timeout"
+done
