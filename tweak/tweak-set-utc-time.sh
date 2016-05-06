b0bfbe32b83c77e9ddc42d26ef167b8fa5376985
geekgit <geeekz@gmail.com>
2016-05-06 23:13:19 +0000

    time tweaks

---
+++ tweak/tweak-set-utc-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "UTC Time"
+
+echo "[timedatectl] Set BIOS time to UTC..."
+sudo timedatectl set-local-rtc 0
+
+echo "/etc/localtime -> /usr/share/zoneinfo/Etc/UTC"
+sudo ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
+
+echo 'echo "Etc/UTC" -> /etc/timezone'
+sudo sh -c 'echo "Etc/UTC" > /etc/timezone"
---
+++ tweak/tweak-set-yekt-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "YEKT Time"
+
+echo "[timedatectl] Set BIOS time to local time..."
+sudo timedatectl set-local-rtc 1
+
+echo "/etc/localtime -> /usr/share/zoneinfo/Etc/UTC"
+sudo ln -sf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
+
+echo 'echo "Etc/UTC" -> /etc/timezone'
+sudo sh -c 'echo "Asia/Yekaterinburg" > /etc/timezone"
