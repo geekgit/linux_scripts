ac51d0459d0d8b3e36b44b96f86d04b892e74e36
geekgit <geeekz@gmail.com>
2016-05-07 01:25:50 +0000

    multiverse

--- install/install-obs-stable-wily.sh
+++ install/install-obs-stable-wily.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "obs-stable / wily"
+sudo add-apt-repository multiverse
 sudo apt-add-repository -y ppa:obsproject/obs-studio
 sudo geekgit-tweak-custom-ppa ppa:obsproject/wily/obs-studio
 sudo apt-get update
--- install/install-obs-unstable.sh
+++ install/install-obs-unstable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "obs-unstable, current version"
+sudo add-apt-repository multiverse
 sudo apt-add-repository -y ppa:obsproject/obs-studio-unstable
 sudo apt-get update
 sudo apt-get upgrade -y
