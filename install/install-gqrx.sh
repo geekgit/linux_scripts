4fd8d12edd20bdfdf7618a29f2646ceaf176ec03
geekgit <geeekz@gmail.com>
2016-05-07 00:08:17 +0000

    fix

--- install/install-gqrx.sh
+++ install/install-gqrx.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 echo "gqrx setup"
 # PPA - wily only
-sudo add-apt-repository -y ppa:bladerf/wily/bladerf
+sudo add-apt-repository -y ppa:bladerf/bladerf
+sudo geekgit-tweak-custom-ppa ppa:bladerf/wily/bladerf
 # PPA - current version
 sudo add-apt-repository -y ppa:ettusresearch/uhd
 # PPA - wily only
-sudo add-apt-repository -y ppa:myriadrf/wily/drivers
+sudo add-apt-repository -y ppa:myriadrf/drivers
+sudo geekgit-tweak-custom-ppa ppa:myriadrf/wily/drivers
 # PPA - wily only
-sudo add-apt-repository -y ppa:myriadrf/wily/gnuradio
+sudo add-apt-repository -y ppa:myriadrf/gnuradio
+sudo geekgit-tweak-custom-ppa ppa:myriadrf/wily/gnuradio
 # PPA - wily only
-sudo add-apt-repository -y ppa:gqrx/wily/gqrx-sdr
+sudo add-apt-repository -y ppa:gqrx/gqrx-sdr
+sudo geekgit-tweak-custom-ppa ppa:gqrx/wily/gqrx-sdr
 # install
 sudo apt-get update
 sudo apt-get upgrade -y
