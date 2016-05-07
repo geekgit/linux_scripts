eed2e4fd4a2050b64e62f98404dedbad891b7d42
geekgit <geeekz@gmail.com>
2016-05-07 01:26:30 +0000

    libfdk-aac fix

--- install/install-mpv-wily.sh
+++ install/install-mpv-wily.sh
@@ -4,4 +4,4 @@ sudo add-apt-repository -y ppa:mc3man/ffmpeg-test1
 sudo geekgit-tweak-custom-ppa ppa:mc3man/wily/ffmpeg-test1
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y ffmpeg mpv audacity fdk-aac libass
+sudo apt-get install -y ffmpeg mpv audacity libfdk-aac1 libfdk-aac-dev libass5
--- install/install-mpv-xenial.sh
+++ install/install-mpv-xenial.sh
@@ -4,4 +4,4 @@ sudo add-apt-repository -y ppa:mc3man/mpv-tests
 sudo geekgit-tweak-custom-ppa ppa:mc3man/xenial/mpv-tests
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y mpv fdk-aac
+sudo apt-get install -y mpv libfdk-aac1 libfdk-aac-dev
