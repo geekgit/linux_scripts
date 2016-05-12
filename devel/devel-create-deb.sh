b5ef5cad35940913d3bfdb72f981289b01af27cb
geekgit <geeekz@gmail.com>
2016-05-12 04:08:10 +0000

    devel-create-deb

---
+++ devel/devel-create-deb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# usage: geekgit-create-deb <Source Path> <Control Path>
+# example: geekgit-create-deb /opt/some-program /root/control
+echo "Create deb struct..."
+PackageName=$(cat control | grep "Package:" | awk '{print $2}')
+echo "Parsed package name: $PackageName"
+FolderName="$PackageName"
+DestPath="./$FolderName/"
+SourcePath="$1"
+ControlPath="$2"
+echo "Source path: $SourcePath"
+echo "control path: $ControlPath"
+mkdir $FolderName
+cp -a "$SourcePath" "$DestPath"
+mkdir "$DestPath/DEBIAN"
+cp "$ControlPath" "$DestPath/DEBIAN"
+dpkg-deb --build "$PackageName"
