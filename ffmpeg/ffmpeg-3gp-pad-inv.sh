62171974369f51fda395b08c97db84afad811dde
geekgit <geeekz@gmail.com>
2016-05-17 03:07:14 +0000

    ffmpeg 3gp pad scripts

---
+++ ffmpeg/ffmpeg-3gp-pad-inv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+Files="$@"
+echo "Files: $Files"
+echo "Loop.."
+Width="352"
+Height="288"
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewNamePad="$Plain-pad.3gp"
+	ffmpeg -y -i $File -r 20 -b 400k -acodec aac -strict experimental -ac 1 -ar 8000 -ab 24k -vf "scale=iw*min($Width/iw\,$Height/ih):ih*min($Width/iw\,$Height/ih), pad=$Width:$Height:($Width-iw*min($Width/iw\,$Height/ih))/2:($Height-ih*min($Width/iw\,$Height/ih))/2" "$NewNamePad"
+done
