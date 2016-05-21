21270e744cc3e8e55e89f26b1128f8103ba1b417
geekgit <geeekz@gmail.com>
2016-05-21 18:08:09 +0000

    ffmpeg-opt scripts

---
+++ ffmpeg/ffmpeg-opt-TV-yuv420p-1080p-nvenc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+Files="$@"
+Width="1920"
+Height="1080"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName="TV-1080p-yuv420p-h264-level40high-crf22-$Plain.mp4"
+	/opt/ffmpeg-3.0.2-opt/bin/ffmpeg -y -i $File -vcodec nvenc_h264 -pix_fmt yuv420p -preset slow -crf 22 -profile:v high -level:v 4.0 -r 24 -vf "scale=iw*min($Width/iw\,$Height/ih):ih*min($Width/iw\,$Height/ih), pad=$Width:$Height:($Width-iw*min($Width/iw\,$Height/ih))/2:($Height-ih*min($Width/iw\,$Height/ih))/2" -acodec aac -strict experimental -ab 320k -ac 2 -ar 48000 $NewName
+done
