4a253fb9d44840d5114cc22e3472861533493283
geekgit <geeekz@gmail.com>
2016-07-07 18:15:29 +0000

    fix sudo cat

--- network/setup-webdav-02.sh
+++ network/setup-webdav-02.sh
@@ -5,4 +5,4 @@ ConfigName="$1"
 ApacheConfigPath="/etc/apache2/apache2.conf"
 ApacheBackup="/etc/apache2/apache2.conf.$(uuidgen).backup"
 sudo cp "$ApacheConfigPath" "$ApacheBackup"
-sudo cat "$ConfigName" >> "$ApacheConfigPath"
+sudo sh -c 'cat "$ConfigName" >> "$ApacheConfigPath"'
