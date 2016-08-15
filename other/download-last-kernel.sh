6a000cf88e81bb871df3636c66a43df80e964368
geekgit <geeekz@gmail.com>
2016-08-15 16:17:36 +0000

    download-last-kernel

---
+++ other/download-last-kernel.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Last update: 2016-08-15 (YYYY-MM-DD)
+function GetLastKernelVersion {
+	Url="http://kernel.ubuntu.com/~kernel-ppa/mainline/"
+	ListFilepath="$HOME/$(uuidgen)-kernel-list.html"
+	wget "$Url" -O "$ListFilepath" > /dev/null 2>&1
+	Link=$(grep -Po '(?<=href=")[^"]*' "$ListFilepath")
+	Kernel=$(echo $Link | tr " " "\n" | tail -n1)
+	Kernel=${Kernel::-1}
+	rm "$ListFilepath"
+	echo $Kernel
+}
+function DownloadKernel {
+	KernelVersion="$1"
+	Arch="$2"
+	Url="http://kernel.ubuntu.com/~kernel-ppa/mainline/$KernelVersion/"
+	KernelFilepath="$HOME/$(uuidgen)-kernel.html"
+	wget "$Url" -O "$KernelFilepath" > /dev/null 2>&1
+	echo "Parsing url '$Url' ..."
+	Link=$(grep -Po '(?<=href=")[^"]*' "$KernelFilepath")
+	GenericArchLink=$(echo $Link | tr " " "\n" | sort | uniq | sort | grep deb | grep "$Arch" | grep generic | tr "\n" " ")
+	HeadersAllLink=$(echo $Link | tr " " "\n" | sort | uniq | sort | grep deb | grep "headers" | grep "all" | tr "\n" " ")
+	AllLinks="CHECKSUMS CHECKSUMS.gpg $GenericArchLink $HeadersAllLink"	
+	echo "Parsed URLs..."
+	OutputFolder="$HOME/$KernelVersion-$(uuidgen)"
+	mkdir "$OutputFolder"
+	for CurrentLink in $AllLinks
+	do
+		AbsoluteUrl="$Url$CurrentLink"
+		echo "Saving $AbsoluteUrl as $OutputFolder/$CurrentLink..."
+		wget "$AbsoluteUrl" -P "$OutputFolder"
+	done
+	cd "$OutputFolder"
+	geekgit-key-import-ubuntu-kernel
+	gpg --verify CHECKSUMS.gpg CHECKSUMS
+	shasum -c CHECKSUMS 2>&1 | grep OK
+}
+Arch="amd64"
+LastKernelVersion=$(GetLastKernelVersion)
+echo "Date: $(date)"
+echo "Architecture: $Arch"
+echo "Last kernel version: $LastKernelVersion"
+DownloadKernel $LastKernelVersion $Arch
