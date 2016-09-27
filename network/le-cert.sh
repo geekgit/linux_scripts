2ec5e532b6e8b653bf513a9eeddcde13b9e55d74
geekgit <geeekz@gmail.com>
2016-09-27 09:31:52 +0000

    le-cert update
    - Path to binary
    - Eval

--- network/le-cert.sh
+++ network/le-cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Server="$1"
 echo "Server: $Server"
-Cmd="letsencrypt certonly --standalone --standalone-supported-challenges http-01 --http-01-port 9999 -d $Server"
+Cmd="/usr/bin/letsencrypt certonly --standalone --standalone-supported-challenges http-01 --http-01-port 9999 -d $Server"
 echo "$Cmd"
-"$Cmd"
+eval "$Cmd"
