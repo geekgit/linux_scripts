ddc29780c167589e18c8df19571d10fc92ffe232
geekgit <geeekz@gmail.com>
2017-02-09 21:11:29 +0000

    1) Autofetch 2) Parse fix

--- kernel/kernel-dl-x86_64-last.sh
+++ kernel/kernel-dl-x86_64-last.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-Kernel=$(geekgit-kernel-get-last-version)
+geekgit-kernel-fetch-list
+Kernel=$(geekgit-kernel-get-last-version | tail -n1)
 echo "Kernel: $Kernel"
 geekgit-kernel-dl-x86_64 "$Kernel"
--- kernel/kernel-dl-x86_64-non-rc.sh
+++ kernel/kernel-dl-x86_64-non-rc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-Kernel=$(geekgit-kernel-get-last-version-non-rc)
+geekgit-kernel-fetch-list
+Kernel=$(geekgit-kernel-get-last-version-non-rc | tail -n1)
 echo "Kernel: $Kernel"
 geekgit-kernel-dl-x86_64 "$Kernel"
