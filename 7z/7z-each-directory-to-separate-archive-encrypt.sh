4b7eae39e2122e5fecd1f5983466155e9fd0d615
geekgit <geeekz@gmail.com>
2017-10-15 15:44:36 +0000

    fix args

--- 7z/7z-each-directory-to-separate-archive-encrypt.sh
+++ 7z/7z-each-directory-to-separate-archive-encrypt.sh
@@ -2,5 +2,7 @@
 Password="$1"
 for D in */
 do
-	7za -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mhc=on -mhe=on -p"$Password" a $D $D
+	DirPath=$(realpath "$D")
+	ArchiveName=${D%/}
+	7za -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mhc=on -mhe=on -p"$Password" a $ArchiveName $DirPath
 done
