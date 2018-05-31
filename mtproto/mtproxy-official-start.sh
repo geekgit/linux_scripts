1ebb9f15f1b723615fee0a0dbcfa4924c4a294fe
geekgit <geeekz@gmail.com>
2018-05-31 15:10:02 +0000

    Fix port.

--- mtproto/mtproxy-official-start.sh
+++ mtproto/mtproxy-official-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ServerPort="443"
 DockerPort="443"
-docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $Port:$DockerPort -d telegrammessenger/proxy
+docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -d telegrammessenger/proxy
 echo "Server port: $ServerPort"
 echo "Docker port: $DockerPort"
