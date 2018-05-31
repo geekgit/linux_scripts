9e694ed514ddad4a6ec6d4bf1e2265c0db667a3a
geekgit <geeekz@gmail.com>
2018-05-31 14:59:08 +0000

    443 port

--- mtproto/mtproxy-official-link.sh
+++ mtproto/mtproxy-official-link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ServerPort="64551"
+ServerPort=$(cat /usr/local/bin/geekgit-mtproxy-official-start | grep ServerPort= | awk -F= '{print $2}' | xargs | tr -d '"' | xargs)
 Secret=$(docker logs official_mtproto_proxy 2>&1 | grep 'Secret 1' | awk -F: '{print $2}' | xargs)
 ExternalIP=$(docker logs official_mtproto_proxy 2>&1 | grep 'External IP' | awk -F: '{print $2}' | xargs)
 echo "tg://proxy?server=$ExternalIP&port=$ServerPort&secret=$Secret"
--- mtproto/mtproxy-official-start.sh
+++ mtproto/mtproxy-official-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ServerPort="64551"
+ServerPort="443"
 DockerPort="443"
 docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $Port:$DockerPort -d telegrammessenger/proxy
 echo "Server port: $ServerPort"
