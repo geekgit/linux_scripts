5e6bd156d6213c80282382b3640e3be46d967741
geekgit <geeekz@gmail.com>
2018-05-31 14:53:39 +0000

    redirect stderr

--- mtproto/mtproxy-official-link.sh
+++ mtproto/mtproxy-official-link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ServerPort="64551"
-Secret=$(docker logs official_mtproto_proxy | grep 'Secret 1' | awk -F: '{print $2}' | xargs)
-ExternalIP=$(docker logs official_mtproto_proxy | grep 'External IP' | awk -F: '{print $2}' | xargs)
+Secret=$(docker logs official_mtproto_proxy 2>&1 | grep 'Secret 1' | awk -F: '{print $2}' | xargs)
+ExternalIP=$(docker logs official_mtproto_proxy 2>&1 | grep 'External IP' | awk -F: '{print $2}' | xargs)
 echo "tg://proxy?server=$ExternalIP&port=$ServerPort&secret=$Secret"
 echo "https://t.me/proxy?server=$ExternalIP&port=$ServerPort&secret=$Secret"
