f099da7df003f0ff8255df31100408aa81a5acae
geekgit <geeekz@gmail.com>
2018-05-31 15:35:00 +0000

    update

--- mtproto/mtproxy-official-start.sh
+++ mtproto/mtproxy-official-start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 ServerPort="443"
 DockerPort="443"
-Secret=$(openssl rand -hex 32)
 mkdir -p ~/mtproxy_official
 mkdir -p ~/mtproxy_official/proxy-config
 MtproxyOfficialDir=$(realpath ~/mtproxy_official)
 MtproxyOfficialProxyConfigDir=$(realpath ~/mtproxy_official/proxy-config)
-SecretPath=$(realpath ~/mtproxy_official/secret.txt)
-echo "$Secret" > $SecretPath
-docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -e SECRET=$Secret -v $MtproxyOfficialProxyConfigDir:/data -d telegrammessenger/proxy
+docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -v $MtproxyOfficialProxyConfigDir:/data -d telegrammessenger/proxy
 echo "Server port: $ServerPort"
 echo "Docker port: $DockerPort"
-echo "Secret: $Secret"
 echo "MTProxy path: $MtproxyOfficialDir"
 echo "proxy-config: $MtproxyOfficialProxyConfigDir"
