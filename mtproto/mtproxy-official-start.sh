c713bc7e473b566ac87e8eb880eb00205ab1e748
geekgit <geeekz@gmail.com>
2018-05-31 15:14:01 +0000

    Random secret.

--- mtproto/mtproxy-official-start.sh
+++ mtproto/mtproxy-official-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ServerPort="443"
 DockerPort="443"
-docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -d telegrammessenger/proxy
+Secret=$(openssl rand -hex 15)
+docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -e SECRET=$Secret -d telegrammessenger/proxy
 echo "Server port: $ServerPort"
 echo "Docker port: $DockerPort"
