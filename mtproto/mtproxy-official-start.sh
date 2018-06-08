3e17a3b7a2c841de6179d37f0de12fb11300a346
geekgit <geeekz@gmail.com>
2018-06-08 08:16:04 +0000

    Tag.

--- mtproto/mtproxy-official-start.sh
+++ mtproto/mtproxy-official-start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ServerPort="8443"
 DockerPort="443"
+Tag="1.0"
 mkdir -p ~/mtproxy_official
 mkdir -p ~/mtproxy_official/proxy-config
 MtproxyOfficialDir=$(realpath ~/mtproxy_official)
 MtproxyOfficialProxyConfigDir=$(realpath ~/mtproxy_official/proxy-config)
-docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -v $MtproxyOfficialProxyConfigDir:/data -d telegrammessenger/proxy
+docker run --name 'official_mtproto_proxy' --restart unless-stopped -p $ServerPort:$DockerPort -v $MtproxyOfficialProxyConfigDir:/data -d telegrammessenger/proxy:$Tag
+echo "Tag: $Tag"
 echo "Server port: $ServerPort"
 echo "Docker port: $DockerPort"
 echo "MTProxy path: $MtproxyOfficialDir"
