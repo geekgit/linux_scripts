6932b3dd8023938d4284d4fd7995c9fcfac6862d
geekgit <geeekz@gmail.com>
2018-10-12 16:55:01 +0000

    fix

--- ffmpeg4/ffmpeg4-TV-audio-stereo-mix-v1-pcm.sh
+++ ffmpeg4/ffmpeg4-TV-audio-stereo-mix-v1-pcm.sh
@@ -12,8 +12,6 @@ do
 	Filename=$(basename "$File")
 	Extension="${Filename##*.}"
 	Plain="${Filename%.*}"
-	NewName="$Plain (ffmpeg $ffmpegVer audio stereo mix v1 TV).mp4"
-	NewNameMKV="$Plain (ffmpeg $ffmpegVer audio stereo mix v1 TV).mkv"
-	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -vcodec copy -acodec pcm_f32le -ac 2 -af "pan=stereo|FL<1.0*FL+0.707*FC+0.707*BL|FR<1.0*FR+0.707*FC+0.707*BR" "$NewName"
-	/opt/$ffmpegOpt/bin/ffmpeg -i "$NewName" -f matroska -vcodec copy -acodec copy "$NewNameMKV"
+	NewNameMKV="$Plain (ffmpeg $ffmpegVer audio stereo mix v1 pcm TV).mkv"
+	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -f matroska -vcodec copy -acodec pcm_f32le -ac 2 -af "pan=stereo|FL<1.0*FL+0.707*FC+0.707*BL|FR<1.0*FR+0.707*FC+0.707*BR" "$NewNameMKV"
 done
--- ffmpeg4/ffmpeg4-TV-audio-stereo-mix-v2-pcm.sh
+++ ffmpeg4/ffmpeg4-TV-audio-stereo-mix-v2-pcm.sh
@@ -12,8 +12,6 @@ do
 	Filename=$(basename "$File")
 	Extension="${Filename##*.}"
 	Plain="${Filename%.*}"
-	NewName="$Plain (ffmpeg $ffmpegVer audio stereo mix v2 TV).mp4"
-	NewNameMKV="$Plain (ffmpeg $ffmpegVer audio stereo mix v2 TV).mkv"
-	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -vcodec copy -acodec pcm_f32le -ac 2 -af "pan=stereo|FL=FC+0.30*FL+0.30*BL|FR=FC+0.30*FR+0.30*BR" "$NewName"
-	/opt/$ffmpegOpt/bin/ffmpeg -i "$NewName" -f matroska -vcodec copy -acodec copy "$NewNameMKV"
+	NewNameMKV="$Plain (ffmpeg $ffmpegVer audio stereo mix v2 pcm TV).mkv"
+	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -f matroska -vcodec copy -acodec pcm_f32le -ac 2 -af "pan=stereo|FL=FC+0.30*FL+0.30*BL|FR=FC+0.30*FR+0.30*BR" "$NewNameMKV"
 done
