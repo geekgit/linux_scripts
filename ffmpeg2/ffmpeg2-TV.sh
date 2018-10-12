1b89b322d198f8eebdb443b43ec6e0f6b924d82f
geekgit <geeekz@gmail.com>
2018-10-12 08:02:48 +0000

    ffmpeg2-TV

---
+++ ffmpeg2/ffmpeg2-TV.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+Files="$@"
+Width="1920"
+Height="1080"
+ffmpegVer="2.8.15"
+ffmpegOpt="ffmpeg-$ffmpegVer-opt"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName="$Plain (ffmpeg $ffmpegVer nvenc TV).mp4"
+	NewNameMKV="$Plain (ffmpeg $ffmpegVer nvenc TV).mkv"
+	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -vcodec nvenc_h264 -pix_fmt yuv420p -acodec aac -strict experimental -ab 320k -ac 2 -ar 48000 "$NewName"
+	/opt/$ffmpegOpt/bin/ffmpeg -i "$NewName" -f matroska -vcodec copy -acodec copy "$NewNameMKV"
+done
