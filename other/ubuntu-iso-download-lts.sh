a84f127babb4b8b5a9adc272f8806be7a3fb63c1
geekgit <geeekz@gmail.com>
2018-10-13 17:20:42 +0000

    ubuntu update

--- other/ubuntu-iso-download-lts.sh
+++ other/ubuntu-iso-download-lts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -r -l1 -t1 -nd -N -np -erobots=off http://releases.ubuntu.com/xenial/
+wget -r -l1 -t1 -nd -N -np -erobots=off http://releases.ubuntu.com/bionic/
--- other/ubuntu-iso-download-stable.sh
+++ other/ubuntu-iso-download-stable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -r -l1 -t1 -nd -N -np -erobots=off http://releases.ubuntu.com/artful/
+#wget -r -l1 -t1 -nd -N -np -erobots=off http://releases.ubuntu.com/cosmic/
+wget -r -l1 -t1 -nd -N -np -erobots=off http://releases.ubuntu.com/bionic/
