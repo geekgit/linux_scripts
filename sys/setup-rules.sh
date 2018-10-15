701b3ca8ff89ffa056f0dc9bc5f9f74614780126
geekgit <geeekz@gmail.com>
2018-10-15 12:09:05 +0000

    setup-rules

---
+++ sys/setup-rules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /etc/udev/rules.d/
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/00-rtlsdr.rules" -O 00-rtlsdr.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/01-temp-sensor.rules" -O 01-temp-sensor.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/02-nakasi.rules" -O 02-nakasi.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/03-iyokan-flash.rules" -O 03-iyokan-flash.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/04-iyokan-fastboot.rules" -O 04-iyokan-fastboot.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/05-iyokan-recovery-sideload-adb.rules" -O 05-iyokan-recovery-sideload-adb.rules
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/udev/rules.d/06-huawei-e1550.rules" -O 06-huawei-e1550.rules
+
