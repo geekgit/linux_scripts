02ed468d4ae0a4db954cee4b0b7137c2252ec2d5
geekgit <geeekz@gmail.com>
2018-11-11 03:54:51 +0000

    txt-broken-utf8-1251 update

--- text/txt-broken-utf8-1251.sh
+++ text/txt-broken-utf8-1251.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 File="$1"
-NewFile0="${File%.*}.1252.txt"
-iconv -f UTF-8 -t CP1252 "${File}" > "${NewFile0}"
-NewFile1="${File%.*}.broken-utf8.txt"
-iconv -f CP1251 -t utf8 "${NewFile0}" > "${NewFile1}"
+NewFile="${File%.*}.broken-utf8.txt"
+iconv -f utf-8 -t CP1252 "${File}" | iconv -f CP1251 -t utf-8 > "${NewFile}"
