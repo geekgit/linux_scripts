962bae7b708aab81544603e0e20638dfbd6a4120
geekgit <geeekz@gmail.com>
2018-12-08 15:42:08 +0000

    scroll tweaks update

--- tweak/tweak-mouse-scroll-set.sh
+++ tweak/tweak-mouse-scroll-set.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-xinput set-prop 10 "libinput Scroll Method Enabled" 0, 0, 1
+function set_scroll {
+	Mouse="$1"
+	xinput set-prop "${Mouse}" "libinput Scroll Method Enabled" 0, 0, 1
+}
+
+function search_mouse {
+	xinput | grep "USB OPTICAL MOUSE" | awk -F= '{print $2}' | awk '{print $1}' | xargs
+}
+
+ID=$(search_mouse)
+for mouse in $ID
+do
+	echo "Mouse: $mouse"
+	set_scroll $mouse
+done
--- tweak/tweak-mouse-scroll-unset.sh
+++ tweak/tweak-mouse-scroll-unset.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-xinput set-prop 10 "libinput Scroll Method Enabled" 0, 0, 0
+function unset_scroll {
+	Mouse="$1"
+	xinput set-prop "${Mouse}" "libinput Scroll Method Enabled" 0, 0, 0
+}
+
+function search_mouse {
+	xinput | grep "USB OPTICAL MOUSE" | awk -F= '{print $2}' | awk '{print $1}' | xargs
+}
+
+ID=$(search_mouse)
+for mouse in $ID
+do
+	echo "Mouse: $mouse"
+	unset_scroll $mouse
+done
