80590e6a7f031ebdef982fdbf204284145634a80
geekgit <geeekz@gmail.com>
2019-05-26 09:37:53 +0000

    firefox-swap-prefs

---
+++ firefox/firefox-swap-prefs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CurrPath="${PWD}"
+MozPath="${HOME}/.mozilla/firefox"
+cd "${MozPath}"
+Dirs=$(ls -ld ./*/ | awk -F/ '{printf "%s ", $2}')
+for curr_dir in $Dirs
+do
+	FullPath="${HOME}/.mozilla/firefox/${curr_dir}"
+	if [ -d "${FullPath}" ]; then
+		echo "${FullPath}"
+		cd "${FullPath}"
+		cp "${HOME}/git/firefox/stable/prefs.js" .
+		cd "${MozPath}" 		
+	fi
+done
