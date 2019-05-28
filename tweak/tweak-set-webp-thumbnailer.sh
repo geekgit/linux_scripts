cb8570d3ed280a77898c21dbc0e9f57496ebe65d
geekgit <geeekz@gmail.com>
2019-05-28 06:51:32 +0000

    tweak-set-webp-thumbnailer

---
+++ tweak/tweak-set-webp-thumbnailer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /usr/share/thumbnailers
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/thumbnailers/webp.thumbnailer" -O webp.thumbnailer
