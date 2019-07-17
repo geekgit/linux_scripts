6614ca02563acd8120b1e52d633bec49d67d243c
geekgit <geeekz@gmail.com>
2019-07-17 21:00:51 +0000

    test-webp: info

--- webp/test-webp.sh
+++ webp/test-webp.sh
@@ -3,7 +3,8 @@ Input="$1"
 Scale1="$2"
 Scale2="$3"
 Output="$4"
-Dimension=$(cwebp "${Input}" | grep "Dimension:" | awk -F: '{print $2}')
+Info=$(/usr/bin/cwebp "${Input}")
+Dimension=$(/usr/bin/cwebp "${Input}" | grep "Dimension:" | awk -F: '{print $2}')
 Size1=$(echo "${Dimension}" | awk -Fx '{print $1}' | xargs)
 Size2=$(echo "${Dimension}" | awk -Fx '{print $2}' | xargs)
 echo "Input: ${Input}" >> ~/test-webp.log
@@ -13,6 +14,7 @@ echo "Output: ${Output}" >> ~/test-webp.log
 echo "Dimension: ${Dimension}" >> ~/test-webp.log
 echo "Size1: ${Size1}" >> ~/test-webp.log
 echo "Size2: ${Size2}" >> ~/test-webp.log
+echo "Info: ${Info}" >> ~/test-webp.log
 /usr/bin/dwebp "${Input}" -scale ${Scale1} ${Scale2} -o "${Output}"
 InputType=$(file "${Input}")
 OutputType=$(file "${Output}")
