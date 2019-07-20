3683c2f7a3add4900f506c2fc281741a638d3852
geekgit <geeekz@gmail.com>
2019-07-20 21:31:40 +0000

    tweak-set-applications: BrowserSelector

--- tweak/tweak-set-applications.sh
+++ tweak/tweak-set-applications.sh
@@ -2,5 +2,7 @@
 cd /usr/share/applications/
 #ffplay
 sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/applications/ffplay.desktop" -O ffplay.desktop
+#BrowserSelector
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/applications/BrowserSelector.desktop" -O BrowserSelector.desktop
 #defaults.list
 sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/applications/defaults.list" -O defaults.list
