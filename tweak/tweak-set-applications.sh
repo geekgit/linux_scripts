af02396c394454a7ee8303e91b3697371f2d8a41
geekgit <geeekz@gmail.com>
2019-07-20 14:32:40 +0000

    tweak-set-applications

---
+++ tweak/tweak-set-applications.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /usr/share/applications/
+#ffplay
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/applications/ffplay.desktop" -O ffplay.desktop
+#defaults.list
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/applications/defaults.list" -O defaults.list
