8530e47aa5e55fe5245af4e3d9fb0634f0e2c6cf
geekgit <geeekz@gmail.com>
2019-10-13 20:19:19 +0000

    txt-1251-to-utf8-batch-replace

---
+++ text/txt-1251-to-utf8-batch-replace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+find ./ -name "*.txt" -type f 2>/dev/null |
+while read txt
+do
+	File="${txt}"
+	NewFile="${File%.*}.utf-8.txt"
+	echo "${File} -> ${NewFile}"
+	iconv -f CP1251 -t UTF-8 "${File}" > "${NewFile}"
+	echo "${NewFile} -> ${File}"
+	mv "${NewFile}" "${File}"
+done
