985be2f7e7aab392521c65bcc83692d148ea3dbc
geekgit <geeekz@gmail.com>
2020-04-25 15:35:11 +0000

    install-jitsi update 2

--- install/install-jitsi.sh
+++ install/install-jitsi.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+create_rw_directory_jitsi()
+{
+	DirName="$1"
+	sudo mkdir -p "${HOME}/.jitsi-meet-cfg/${DirName}"
+	sudo chown "${USER}":"${USER}" "${HOME}/.jitsi-meet-cfg/${DirName}"
+	sudo chmod a+rwx "${HOME}/.jitsi-meet-cfg/${DirName}"
+}
 sudo apt-get install -y docker-compose
 cd $HOME
 git clone https://github.com/jitsi/docker-jitsi-meet
@@ -6,13 +13,13 @@ cd $HOME/docker-jitsi-meet
 cat env.example > .env
 ./gen-passwords.sh
 cd $HOME
-mkdir -p $HOME/.jitsi-meet-cfg/web
-mkdir -p $HOME/.jitsi-meet-cfg/letsencrypt
-mkdir -p $HOME/.jitsi-meet-cfg/transcripts
-mkdir -p $HOME/.jitsi-meet-cfg/prosody
-mkdir -p $HOME/.jitsi-meet-cfg/jicofo
-mkdir -p $HOME/.jitsi-meet-cfg/jvb
-mkdir -p $HOME/.jitsi-meet-cfg/jigasi
-mkdir -p $HOME/.jitsi-meet-cfg/jibri
+create_rw_directory_jitsi web
+create_rw_directory_jitsi letsencrypt
+create_rw_directory_jitsi transcripts
+create_rw_directory_jitsi prosody
+create_rw_directory_jitsi jicofo
+create_rw_directory_jitsi jvb
+create_rw_directory_jitsi jigasi
+create_rw_directory_jitsi jibri
 cd $HOME/docker-jitsi-meet
 docker-compose up -d
