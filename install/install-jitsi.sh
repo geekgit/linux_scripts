312d7f39267f1a761cfc292d95d461064b3e4ca0
geekgit <geeekz@gmail.com>
2020-04-25 15:28:28 +0000

    install-jitsi update

--- install/install-jitsi.sh
+++ install/install-jitsi.sh
@@ -2,9 +2,10 @@
 sudo apt-get install -y docker-compose
 cd $HOME
 git clone https://github.com/jitsi/docker-jitsi-meet
-cd docker-jitsi-meet
+cd $HOME/docker-jitsi-meet
 cat env.example > .env
 ./gen-passwords.sh
+cd $HOME
 mkdir -p $HOME/.jitsi-meet-cfg/web
 mkdir -p $HOME/.jitsi-meet-cfg/letsencrypt
 mkdir -p $HOME/.jitsi-meet-cfg/transcripts
@@ -13,4 +14,5 @@ mkdir -p $HOME/.jitsi-meet-cfg/jicofo
 mkdir -p $HOME/.jitsi-meet-cfg/jvb
 mkdir -p $HOME/.jitsi-meet-cfg/jigasi
 mkdir -p $HOME/.jitsi-meet-cfg/jibri
+cd $HOME/docker-jitsi-meet
 docker-compose up -d
