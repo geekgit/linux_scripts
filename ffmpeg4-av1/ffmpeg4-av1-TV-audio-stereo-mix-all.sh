441eb36160ffb8f7228051faffcf2135f81e4c57
geekgit <geeekz@gmail.com>
2020-05-14 13:40:48 +0000

    ffmpeg4 av1

---
+++ ffmpeg4-av1/ffmpeg4-av1-TV-audio-stereo-mix-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Files="$@"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	geekgit-ffmpeg4-av1-TV-audio-stereo-mix-v1 "$Filename"
+	geekgit-ffmpeg4-av1-TV-audio-stereo-mix-v2 "$Filename"
+done
---
+++ ffmpeg4-av1/ffmpeg4-av1-TV-xvid-low.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+Files="$@"
+Width="1920"
+Height="1080"
+ffmpegVer="4.2.2"
+ffmpegOpt="ffmpeg-$ffmpegVer-av1-opt"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName="$Plain (ffmpeg $ffmpegVer xvid low TV).avi"
+	NewNameMKV="$Plain (ffmpeg $ffmpegVer xvid low TV).mkv"
+	/opt/$ffmpegOpt/bin/ffmpeg -y -i $File -sn -c:a libmp3lame -ar 48000 -ab 128k -ac 2 -c:v libxvid -crf 24 -vtag DIVX -mbd rd -flags +mv4+aic -trellis 2 -cmp 2 -subcmp 2 -g 30 -vb 1500k "$NewName"
+	/opt/$ffmpegOpt/bin/ffmpeg -i "$NewName" -f matroska -vcodec copy -acodec copy "$NewNameMKV"
+done
