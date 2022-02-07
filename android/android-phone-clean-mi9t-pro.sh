05dbe82744b8c09e46d6eaa0a6ccbad05703e5a5
geekgit <geeekz@gmail.com>
2022-02-07 04:26:03 +0000

    android-phone-clean-mi9t-pro update

--- android/android-phone-clean-mi9t-pro.sh
+++ android/android-phone-clean-mi9t-pro.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 adb_uninstall_xiaomi() {
 	ApkName="$1"
+	echo "$1:"
 	adb shell pm uninstall -k --user 0 "${ApkName}"
+	echo "==="
 }
 # Power
 #adb_uninstall_xiaomi com.miui.powerkeeper
@@ -37,3 +39,34 @@ adb_uninstall_xiaomi com.facebook.katana
 adb_uninstall_xiaomi com.facebook.orca
 adb_uninstall_xiaomi com.facebook.system
 adb_uninstall_xiaomi com.facebook.services
+# ru apps
+adb_uninstall_xiaomi ru.ozon.app.android
+adb_uninstall_xiaomi ru.yandex.searchplugin
+adb_uninstall_xiaomi com.allgoritm.youla
+adb_uninstall_xiaomi com.yandex.zen
+# more oem
+adb_uninstall_xiaomi com.mi.globalbrowser
+adb_uninstall_xiaomi com.miui.miservice
+adb_uninstall_xiaomi com.miui.compass
+adb_uninstall_xiaomi com.xiaomi.scanner
+adb_uninstall_xiaomi com.zhiliaoapp.musically
+adb_uninstall_xiaomi com.opera.preinstall
+adb_uninstall_xiaomi com.opera.browser
+adb_uninstall_xiaomi com.miui.notes
+adb_uninstall_xiaomi com.miui.videoplayer
+adb_uninstall_xiaomi com.miui.player
+adb_uninstall_xiaomi com.xiaomi.midrop
+adb_uninstall_xiaomi com.xiaomi.calendar
+adb_uninstall_xiaomi com.miui.weather2
+adb_uninstall_xiaomi com.alibaba.aliexpresshd
+adb_uninstall_xiaomi com.xiaomi.mipicks
+# some android stuff
+adb_uninstall_xiaomi com.android.providers.downloads.ui
+adb_uninstall_xiaomi com.google.android.apps.photos
+adb_uninstall_xiaomi com.google.android.apps.tachyon
+adb_uninstall_xiaomi com.google.android.videos
+adb_uninstall_xiaomi com.google.android.youtube
+#adb_uninstall_xiaomi com.google.android.gm
+adb_uninstall_xiaomi com.android.chrome
+adb_uninstall_xiaomi com.android.thememanager
+adb_uninstall_xiaomi com.android.thememanager.module
