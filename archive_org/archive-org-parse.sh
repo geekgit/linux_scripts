cf2f11ae6d53e80dc29ffd6af01ce10b537f2c87
geekgit <geeekz@gmail.com>
2024-05-07 14:52:46 +0000

    Update archive-org-parse.sh

--- archive_org/archive-org-parse.sh
+++ archive_org/archive-org-parse.sh
@@ -3,7 +3,7 @@ Link="$1"
 LinksRelativeFile="$(uuidgen)_links_relative.txt"
 LinksAbsoluteFile="$(uuidgen)_links_absolute.txt"
 
-curl -s "${Link}" | grep -E -io 'href="[^\"]+"' | awk -F\" '{print$2}' | grep -Ev "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep -v "/" | grep -v "#" >> "${LinksRelativeFile}" | grep -v "_archive.torrent" | grep -v "_files.xml" | grep -v "_meta.sqlite" | grep -v "_meta.xml"
+curl -s "${Link}" | grep -E -io 'href="[^\"]+"' | awk -F\" '{print$2}' | grep -Ev "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep -v "/" | grep -v "#" | grep -v "_archive.torrent" | grep -v "_files.xml" | grep -v "_meta.sqlite" | grep -v "_meta.xml" >> "${LinksRelativeFile}" 
 
 
 while IFS= read -r link; do
