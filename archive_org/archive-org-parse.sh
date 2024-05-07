9d0faf177790704a49925b2953650f343366f857
geekgit <geeekz@gmail.com>
2024-05-07 14:06:42 +0000

    Update archive-org-parse.sh

--- archive_org/archive-org-parse.sh
+++ archive_org/archive-org-parse.sh
@@ -1,3 +1,10 @@
-﻿#!/bin/bash
+#!/bin/bash
 Link="$1"
-curl -s "${Link}" | grep -E -io 'href="[^\"]+"' | awk -F\" '{print$2}' | grep -Ev "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep -v "/" | grep -v "#"
+LinksRelativeFile="$(uuidgen)_links_relative.txt"
+LinksAbsoluteFile="$(uuidgen)_links_absolute.txt"
+
+curl -s "${Link}" | grep -E -io 'href="[^\"]+"' | awk -F\" '{print$2}' | grep -Ev "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep -v "/" | grep -v "#" >> "${LinksRelativeFile}"
+
+while IFS= read -r link; do
+	echo "${Link}/${link}" >> "${LinksAbsoluteFile}"
+done < "${LinksRelativeFile}"
