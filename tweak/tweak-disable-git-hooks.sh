9aa3c7c64153866d54f5810136dd02e75a237c77
geekgit <geeekz@gmail.com>
2024-05-18 00:53:41 +0000

    Update tweak-disable-git-hooks.sh

--- tweak/tweak-disable-git-hooks.sh
+++ tweak/tweak-disable-git-hooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$HOME"
 rm .gitconfig
-wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/home/username/nohooks.gitconfig" -O .gitconfig
+wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/home/username/secure_linux.gitconfig" -O .gitconfig
 sudo chmod a+r-wx .gitconfig
 sudo chattr +i .gitconfig
