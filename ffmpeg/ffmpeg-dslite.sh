6790bfa538b096ecf5d31bd7e642258395e41549
geekgit <geeekz@gmail.com>
2024-05-18 02:21:24 +0000

    Create ffmpeg-dslite.sh

---
+++ ffmpeg/ffmpeg-dslite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Source: https://gbatemp.net/threads/mpeg4-player-for-ds-and-dsi.544095/
+Files="$@"
+echo "Files: $Files"
+echo "Loop.."
+for File in $Files
+do
+	echo "File: $File"
+	Filename=$(basename "$File")
+	Extension="${Filename##*.}"
+	Plain="${Filename%.*}"
+	NewName="dslite-$Plain.mp4"
+	ffmpeg -i $File -f mp4 -s 176x144 -vf "colorspace=space=ycgco:primaries=bt709:trc=bt709:range=pc:iprimaries=bt709:iall=bt709" -dst_range 1 -color_range 2 -vcodec mpeg4 -profile:v 0 -level 8 -b:v 350000 -acodec aac -ar 22050 -b:a 64000 -ac 1 -slices 1 -g 50 $NewName
+done
