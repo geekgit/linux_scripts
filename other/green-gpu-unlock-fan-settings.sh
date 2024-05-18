299c91abcd45561eb53dc8ad7389c51fc38735e7
geekgit <geeekz@gmail.com>
2024-05-18 23:42:58 +0000

    clean

--- other/green-gpu-unlock-fan-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-function _run
-{
-	Cmd="$(echo "$1" | base64 -d)"
-	echo "${1} -> ${Cmd}"
-	eval "$Cmd"
-}
-_run "c3VkbyBudmlkaWEteGNvbmZpZwo="
-_run c3VkbyBudmlkaWEteGNvbmZpZyAtLWNvb2wtYml0cz00Cg==
