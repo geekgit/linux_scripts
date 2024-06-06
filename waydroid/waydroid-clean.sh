9b89601d9ee32c6ed8687f878858028c33413cd7
geekgit <geeekz@gmail.com>
2024-06-06 16:26:28 +0000

    Create waydroid-clean.sh

---
+++ waydroid/waydroid-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo rm -rf /var/lib/waydroid
+sudo rm -rf /home/.waydroid
+sudo rm -rf ~/.waydroid
+sudo rm -rf ~/waydroid
+sudo rm -rf ~/.share/waydroid
+sudo rm -rf ~/.local/share/applications/*aydroid*
+sudo rm -rf ~/.local/share/waydroid
