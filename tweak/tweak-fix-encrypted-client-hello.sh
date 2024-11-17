1281bbf0299f57e87b4a06620ecc185078d5e4d7
geekgit <geeekz@gmail.com>
2024-11-17 09:03:26 +0000

    Update tweak-fix-encrypted-client-hello.sh

--- tweak/tweak-fix-encrypted-client-hello.sh
+++ tweak/tweak-fix-encrypted-client-hello.sh
@@ -13,8 +13,11 @@ then
 		cd linux_configs
 		sudo mkdir -p /etc/opt/chrome/policies/managed/
 		sudo cp ./etc/opt/chrome/policies/managed/ech-disabled.json /etc/opt/chrome/policies/managed/ech-disabled.json
+		sudo mkdir -p /etc/opt/chromium/policies/managed/
 		sudo cp ./etc/opt/chromium/policies/managed/ech-disabled.json /etc/opt/chromium/policies/managed/ech-disabled.json
+		sudo mkdir -p /etc/opt/chromium-browser/policies/managed/
 		sudo cp ./etc/opt/chromium-browser/policies/managed/ech-disabled.json /etc/opt/chromium-browser/policies/managed/ech-disabled.json
+		sudo mkdir -p /etc/opt/brave/policies/managed/
 		sudo cp ./etc/opt/brave/policies/managed/ech-disabled.json /etc/opt/brave/policies/managed/ech-disabled.json
 		cd "${CurrPath}"
 		rm -rf "${TmpPath}"
